2ac3d347d357caf2a1053c3685ee0cee831091df
Savan Trada <savantrada1022@gmail.com>
2019-05-30 02:31:57 -0400

    Updates

--- precache-manifest.d62fdb73ac0841491a813ad415f40858.js
+++ precache-manifest.d62fdb73ac0841491a813ad415f40858.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "16a64c0a50d282389ac7ebfbbfeec342",
+    "revision": "8402334f707960e94926eb3cdc27e156",
     "url": "/savanportfolio/index.html"
   },
   {
-    "revision": "5aa74a154c5447b39ab2",
+    "revision": "3e8cae460e43e8c3957f",
     "url": "/savanportfolio/static/css/main.0d138f89.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/savanportfolio/static/js/2.cab437bf.chunk.js"
   },
   {
-    "revision": "5aa74a154c5447b39ab2",
-    "url": "/savanportfolio/static/js/main.0e0fa0f5.chunk.js"
+    "revision": "3e8cae460e43e8c3957f",
+    "url": "/savanportfolio/static/js/main.d44bd1f6.chunk.js"
   },
   {
     "revision": "f2d8d4f41f54cd8d213e",
