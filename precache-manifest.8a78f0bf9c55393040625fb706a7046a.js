b9562c76e0e0ea665f66a248a166924899e76ce0
Savan Trada <savantrada1022@gmail.com>
2019-05-30 00:45:46 -0400

    Updates

---
+++ precache-manifest.8a78f0bf9c55393040625fb706a7046a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee577e2d38176de4365878a439010aa8",
+    "url": "/savanportfolio/index.html"
+  },
+  {
+    "revision": "5e5a8ce2c9b43af1cdc4",
+    "url": "/savanportfolio/static/css/main.f3694da6.chunk.css"
+  },
+  {
+    "revision": "9b562924b5f0ffc3a03f",
+    "url": "/savanportfolio/static/js/2.cab437bf.chunk.js"
+  },
+  {
+    "revision": "5e5a8ce2c9b43af1cdc4",
+    "url": "/savanportfolio/static/js/main.fc69841a.chunk.js"
+  },
+  {
+    "revision": "f2d8d4f41f54cd8d213e",
+    "url": "/savanportfolio/static/js/runtime~main.cbe512dd.js"
+  }
+]);
