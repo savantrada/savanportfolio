ce1522c713887f1f8583731c4d48a1901fd708f2
Savan Trada <savantrada1022@gmail.com>
2019-05-30 00:40:36 -0400

    gh pages

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -3,7 +3,10 @@ module.exports = {
     staticFileGlobs: [
       'build/*.html',
       'build/manifest.json',
-      'build/static/**/!(*map*)'
+      'build/static/**/!(*map*)',
+      'build/documents/Savankumar_Trada_Resume.pdf',
+      'build/images/!(*map*)',
+      'build/libs/css/bulma-timeline.min.css'
     ],
     dontCacheBustUrlsMatching: /\.\w{8}\./,
     swFilePath: 'build/service-worker.js'
